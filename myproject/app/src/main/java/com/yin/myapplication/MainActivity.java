83d8ab2a4cb657d55a9ce0f904a322e0f2e4e91c
youngyin <wjswjs6879@naver.com>
2021-06-11 12:08:05 +0900

    connect server

--- myproject/app/src/main/java/com/yin/myapplication/MainActivity.java
+++ myproject/app/src/main/java/com/yin/myapplication/MainActivity.java
@@ -1,19 +1,36 @@
 package com.yin.myapplication;
 
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.yin.myapplication.databinding.ActivityMainBinding;
 import com.yin.myapplication.db.UserDBManager;
 
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class MainActivity extends AppCompatActivity {
     ActivityMainBinding binding;
+    static Context context;
+    String result_connect ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,34 +38,34 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // SharedPreferences - user
-        UserDBManager userDBManager = new UserDBManager(getApplicationContext());
+        context = getApplicationContext();
+        UserDBManager userDBManager = new UserDBManager(context);
 
         // 뷰 바인딩
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         binding.setLifecycleOwner(this);
 
         binding.buttonLogin.setOnClickListener(v->{
-            // 로그인 성공 시
-            UserDBManager.setUser_id(binding.userId.getText().toString());
-            UserDBManager.setUser_pw(binding.userPw.getText().toString());
-            UserDBManager.setPhone(binding.userPhone.getText().toString());
-            Intent intent = new Intent(this, MenuActivity.class);
-            startActivity(intent);
+            String mUrl = "http://10.0.2.2:3000/api/get/login/"+
+                    binding.userId.getText().toString()+"/" +
+                    binding.userPw.getText().toString() + "/" +
+                    binding.userPhone.getText().toString();
+            try {
+                new JSONTask().execute(mUrl);
+                String result = binding.connect.getText().toString();
 
-            // 로그인 실패 시
-            AlertDialog.Builder dlg = new AlertDialog.Builder(MainActivity.this);
-            dlg.setTitle("로그인 실패"); //제목
-            dlg.setMessage("존재하지 않는 회원 정보입니다. 회원가입하시겠습니까?"); // 메시지
-            dlg.setPositiveButton("확인", new DialogInterface.OnClickListener(){
-                public void onClick(DialogInterface dialog, int which) {
-                    binding.buttonSignup.setVisibility(View.VISIBLE);
-                    binding.buttonLogin.setVisibility(View.GONE);
-                }
-            });
-            dlg.show();
+            } catch (Exception e){
+                binding.connect.setText("잘못된 계정입니다.");
+            }
         });
 
         binding.buttonSignup.setOnClickListener(v -> {
+            String mUrl = "http://10.0.2.2:3000/api/get/signup/"+
+                    binding.userId.getText().toString()+"/" +
+                    binding.userPw.getText().toString() + "/" +
+                    binding.userPhone.getText().toString();
+            new JSONTask().execute(mUrl);
+
             UserDBManager.setUser_id(binding.userId.getText().toString());
             UserDBManager.setUser_pw(binding.userPw.getText().toString());
             UserDBManager.setPhone(binding.userPhone.getText().toString());
@@ -61,5 +78,106 @@ public class MainActivity extends AppCompatActivity {
         binding.userId.setText(UserDBManager.getUser_id());
         binding.userPw.setText(UserDBManager.getUser_pw());
         binding.userPhone.setText(UserDBManager.getPhone());
+    }
+
+    private void s_login(){
+        // 로그인 성공 시
+        UserDBManager.setUser_id(binding.userId.getText().toString());
+        UserDBManager.setUser_pw(binding.userPw.getText().toString());
+        UserDBManager.setPhone(binding.userPhone.getText().toString());
+        Intent intent = new Intent(MainActivity.context, MenuActivity.class);
+        startActivity(intent);
+    }
+
+    private void f_login(){
+        // 로그인 실패 시
+        AlertDialog.Builder dlg = new AlertDialog.Builder(MainActivity.context);
+        dlg.setTitle("로그인 실패"); //제목
+        dlg.setMessage("존재하지 않는 회원 정보입니다. 회원가입하시겠습니까?"); // 메시지
+        dlg.setPositiveButton("확인", new DialogInterface.OnClickListener(){
+            public void onClick(DialogInterface dialog, int which) {
+                binding.buttonSignup.setVisibility(View.VISIBLE);
+                binding.buttonLogin.setVisibility(View.GONE);
+            }
+        });
+        dlg.show();
+    }
+
+    public class JSONTask extends AsyncTask<String, String, String> {
+
+        @Override
+
+        protected String doInBackground(String... urls) {
+
+            try {
+                HttpURLConnection con = null;
+                BufferedReader reader = null;
+
+                try{
+
+                    URL url = new URL(urls[0]);//url을 가져온다.
+                    con = (HttpURLConnection) url.openConnection();
+                    con.connect();//연결 수행
+                    //입력 스트림 생성
+
+                    InputStream stream = con.getInputStream();
+                    //속도를 향상시키고 부하를 줄이기 위한 버퍼를 선언한다.
+                    reader = new BufferedReader(new InputStreamReader(stream));
+
+                    //실제 데이터를 받는곳
+                    StringBuffer buffer = new StringBuffer();
+
+                    //line별 스트링을 받기 위한 temp 변수
+                    String line = "";
+
+                    //아래라인은 실제 reader에서 데이터를 가져오는 부분이다. 즉 node.js서버로부터 데이터를 가져온다.
+                    while((line = reader.readLine()) != null){
+                        buffer.append(line);
+                    }
+
+                    //다 가져오면 String 형변환을 수행한다. 이유는 protected String doInBackground(String… urls) 니까
+                    return buffer.toString();
+
+                    //아래는 예외처리 부분이다.
+
+                } catch (MalformedURLException e){
+                    e.printStackTrace();
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+
+                } finally {
+
+                    //종료가 되면 disconnect메소드를 호출한다.
+                    if(con != null){
+                        con.disconnect();
+                    }
+
+                    try {
+                        //버퍼를 닫아준다.
+                        if(reader != null){
+                            reader.close();
+                        }
+
+                    } catch (IOException e) {
+                        e.printStackTrace();
+
+                    }
+
+                }//finally 부분
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        //doInBackground메소드가 끝나면 여기로 와서 텍스트뷰의 값을 바꿔준다.
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            binding.connect.setText(result);
+        }
+
     }
 }
